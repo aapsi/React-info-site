ced1fd92488effa0fac982b4d2eb6886bf9a47d3
aapsi <aapsi0001@gmail.com>
2023-03-01 15:55:37 +0530

    site-complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,12 @@
-/**
-Challenge: Complete the Navbar to match the design
-
-Hint: use the Figma file for the most accurate peek at the design
-(colors, sizes, fonts, etc.)
-*/
-
 import React from "react"
+import myimg from "../images/react-logo.png"
 
 export default function Navbar() {
     return (
-        <nav>
-            <img src="./images/react-icon-small.png" className="nav--icon" alt=""/>
+        <nav className="navbar">
+            <img src= {myimg} className="nav--icon" alt="react"/>
             <h3 className="nav--logo_text">ReactFacts</h3>
             <h4 className="nav--title">React Course - Project 1</h4>
-        </nav>
+        </nav>    
     )
 }
