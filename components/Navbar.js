f41be987201b3b23a1f7d77cfcc89ed9ea1aef25
aapsi <aapsi0001@gmail.com>
2023-03-01 15:49:55 +0530

    info site

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,12 +1,12 @@
 import React from "react"
-import myimg from "./src/images/react-icon-small.png"
+import myimg from "./images/react-logo.png"
 
 export default function Navbar() {
     return (
         <nav className="navbar">
-            <img src= {myimg} className="nav--icon" alt="react"/>
-            <h3>ReactFacts</h3>
-            <h4>React Course - Project 1</h4>
+            <img src= {myimg} className="nav--icon" alt="react-logo"/>
+            <h3 calssName="nav--logo_text">ReactFacts</h3>
+            <h4 className="nav--title">React Course - Project 1</h4>
         </nav>    
     )
 }
